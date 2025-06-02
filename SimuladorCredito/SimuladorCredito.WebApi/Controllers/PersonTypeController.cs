8684451835689f6bc01c07419b8a1f89d11af99d
Kalebe Fernandes <kallebe.fernandes@hotmail.com>
2025-06-02 09:19:18 -0300

    Cria as controllers

---
+++ SimuladorCredito/SimuladorCredito.WebApi/Controllers/PersonTypeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimuladorCredito.WebApi.Controllers
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    [Produces("aplication/json")]
+    [ApiConventionType(typeof(DefaultApiConventions))]
+    public class PersonTypeController : ControllerBase
+    {
+    }
+}
---
+++ SimuladorCredito/SimuladorCredito.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimuladorCredito.WebApi.Controllers
+{
+    [Route("api/v{version:apiversion}/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    [ApiConventionType(typeof(DefaultApiConventions))]
+    public class ProductController : ControllerBase
+    {
+    }
+}
